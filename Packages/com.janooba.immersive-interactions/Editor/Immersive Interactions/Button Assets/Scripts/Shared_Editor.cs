b92c2bd0cb3d6a7b682c0f9321293da4ca75fb20
Kolton Meier <Kolton.Meier@gmail.com>
2023-09-16 18:48:49 -0600

    increment version

--- Packages/com.janooba.immersive-interactions/Editor/Immersive Interactions/Button Assets/Scripts/Shared_Editor.cs
+++ Packages/com.janooba.immersive-interactions/Editor/Immersive Interactions/Button Assets/Scripts/Shared_Editor.cs
@@ -7,7 +7,7 @@ namespace JanoobaAssets
 {
     public static class Shared_EditorUtility
     {
-        public const string VERSION = "v0.2.0";
+        public const string VERSION = "v0.2.1";
         
         private static GUIStyle _boldHeader = null;
         public static GUIStyle BoldHeader
