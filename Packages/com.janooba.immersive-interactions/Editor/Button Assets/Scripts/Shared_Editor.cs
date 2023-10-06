83b6f2594a29c1515fd0f91c698cab027a8cde6e
Janooba <me@Janooba.com>
2023-10-05 21:02:39 -0600

    Fixed missing out sounds for button prefabs

--- Packages/com.janooba.immersive-interactions/Editor/Button Assets/Scripts/Shared_Editor.cs
+++ Packages/com.janooba.immersive-interactions/Editor/Button Assets/Scripts/Shared_Editor.cs
@@ -7,7 +7,7 @@ namespace JanoobaAssets
 {
     public static class Shared_EditorUtility
     {
-        public const string VERSION = "v0.2.6-beta.1";
+        public const string VERSION = "v0.2.6-beta.2";
         
         private static GUIStyle _boldHeader = null;
         public static GUIStyle BoldHeader
