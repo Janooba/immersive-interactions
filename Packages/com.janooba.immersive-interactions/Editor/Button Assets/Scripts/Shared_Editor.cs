3d75fbb76783cdf63da2ff2516616ae9e320df0b
Janooba <Kolton.Meier@gmail.com>
2024-08-04 20:18:53 -0600

    editor version bump

--- Packages/com.janooba.immersive-interactions/Editor/Button Assets/Scripts/Shared_Editor.cs
+++ Packages/com.janooba.immersive-interactions/Editor/Button Assets/Scripts/Shared_Editor.cs
@@ -7,7 +7,7 @@ namespace JanoobaAssets
 {
     public static class Shared_EditorUtility
     {
-        public const string VERSION = "v0.4.0-beta.4";
+        public const string VERSION = "v0.4.0-beta.5";
         
         private static GUIStyle _boldHeader = null;
         public static GUIStyle BoldHeader
